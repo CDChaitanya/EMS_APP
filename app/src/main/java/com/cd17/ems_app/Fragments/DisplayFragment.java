1df42f232eb7e408ce5d2e58bd56993e65d4490e
Nandini Deore <nandinideore05@gmail.com>
2020-11-11 16:50:16 +0530

    Display Fragment

--- app/src/main/java/com/cd17/ems_app/Fragments/DisplayFragment.java
+++ app/src/main/java/com/cd17/ems_app/Fragments/DisplayFragment.java
@@ -1,23 +1,68 @@
 package com.cd17.ems_app.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.cd17.ems_app.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class DisplayFragment extends Fragment
 {
+
+    private ListView listView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_display, container, false);
+        View view = inflater.inflate(R.layout.fragment_display, container, false);
+        listView = view.findViewById(R.id.listView);
+        ArrayList<String> list = new ArrayList<String>();
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.list_item,list);
+        listView.setAdapter(adapter);
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Employees");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                list.clear();
+                for(DataSnapshot snap : dataSnapshot.getChildren()){
+                   /* Employees emp = dataSnapshot.getValue(Employees.class);
+                    String txt = emp.getEmp_id() + " : " + emp.getName();*/
+                    list.add(txt);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Intent intent = new Intent(getActivity(),DisplayFragment.class);
+
+            }
+        });
+
+        return view;
     }
 }
