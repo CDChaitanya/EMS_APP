f8a87424c8b9525a167647dfa700bc1fec341007
Nandini Deore <nandinideore05@gmail.com>
2020-11-12 13:13:39 +0530

    Filter Fragment

--- app/src/main/java/com/cd17/ems_app/Fragments/FilterFragment.java
+++ app/src/main/java/com/cd17/ems_app/Fragments/FilterFragment.java
@@ -7,17 +7,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.cd17.ems_app.R;
 
 public class FilterFragment extends Fragment
 {
+    private ListView lv;
+    String items[] = new String [] {"Year of Experience","City","Department","Role"};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_filter, container, false);
+        View v = inflater.inflate(R.layout.fragment_filter, container, false);
+
+        lv = v.findViewById(R.id.lv);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.list_item,items);
+
+        return v;
     }
 }
